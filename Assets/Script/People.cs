f600899ab431e8f35749ce9f10ab013153069ec9
Darmawan Saputra <rpldarmawan@gmail.com>
2019-09-25 02:10:03 +0700

    Add pickup people

---
+++ Assets/Script/People.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class People : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SpriteRenderer p = FindObjectOfType<Player>().GetComponent<SpriteRenderer>();
+        p.color = new Color(238f / 255f, 53f / 255f, 224f / 255f);
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Script/Spawner.cs
+++ Assets/Script/Spawner.cs
@@ -27,7 +27,7 @@ public class Spawner : MonoBehaviour
         while (true)
         {
             GameObject.Instantiate(car, transform.position, Quaternion.identity);
-            yield return new WaitForSeconds(1.0f);
+            yield return new WaitForSeconds(2.0f);
 
         }
     }
