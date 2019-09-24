f6a1e07bbfcdbde0e92c3c2214e218a70240940d
Darmawan Saputra <rpldarmawan@gmail.com>
2019-09-25 06:57:00 +0700

    Add score system

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     Rigidbody2D rb;
     public float speed;
+    SpriteRenderer render;
+    public Text textScore;
+
+    int score = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        render = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -21,5 +28,17 @@ public class Player : MonoBehaviour
         tempVect = tempVect.normalized * speed * Time.deltaTime;
         rb.MovePosition(rb.transform.position + tempVect);
 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Goal" && render.color == new Color(238f / 255f, 53f / 255f, 224f / 255f))
+        {
+            render.color = new Color(255f / 255f, 107f / 255f, 93f / 255f);
+            FindObjectOfType<PeopleSpawner>().Spawn();
+            score++;
+
+            textScore.text = score + "";
+        }
     }
 }
