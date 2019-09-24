e47c5f5e985ea1f4624e497af0f93ca249434c4c
Darmawan Saputra <rpldarmawan@gmail.com>
2019-09-25 01:21:22 +0700

    Add people spawner point

---
+++ Assets/Script/PeopleSpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PeopleSpawner : MonoBehaviour
+{
+    public GameObject peopleObject;
+    public bool isExist = false;
+
+    void Start()
+    {
+        if(!isExist)
+        {
+            Spawn();
+        }
+    }
+
+    public void Spawn()
+    {
+        isExist = true;
+        Instantiate(peopleObject, transform.position, Quaternion.identity);
+    }
+}
