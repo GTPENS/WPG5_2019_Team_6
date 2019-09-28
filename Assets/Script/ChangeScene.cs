1680556452afc9252f77d49ba8e0c02e9ecf6807
LahatArmy <mayvangandhy@gmail.com>
2019-09-28 09:08:41 +0700

    MainMenu

---
+++ Assets/Script/ChangeScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Game()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Credit()
+    {
+        SceneManager.LoadScene("Credit");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
