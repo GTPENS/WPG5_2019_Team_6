e36dd034bbab119c2572f18ea1ee6486ee612109
LahatArmy <mayvangandhy@gmail.com>
2019-12-05 08:39:50 +0700

    Add AI and Fix Camera

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -39,7 +39,8 @@ public class Player : MonoBehaviour
             transform.localScale = temp;
             
         }
-       
+        
+
 
         Vector3 tempVect = new Vector3(moveHorizontal, moveVertical, 0);
         tempVect = tempVect.normalized * speed * Time.deltaTime;
@@ -64,6 +65,7 @@ public class Player : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
         if(collision.tag == "Goal" && amount > 0)
         {
             render.color = new Color(255f / 255f, 107f / 255f, 93f / 255f);
